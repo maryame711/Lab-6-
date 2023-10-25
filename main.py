d163554ab6108c565613b44df0501150cd1a8548
maryame711 <149011568+maryame711@users.noreply.github.com>
2023-10-25 16:32:55 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,19 @@
+github = ''
+
+def encode(password):
+    encode_list = []
+    for digit in password:
+        encode_list.append(int(digit))
+
+    for i in range(0,len(encode_list)):
+        encode_list[i] = encode_list[i] + 3
+
+    for j in range(0, len(encode_list)):
+        encode_list[i] = str(encode_list[i])
+    enc = "".join(encode_list)
+    global github
+    github = enc
+    print("Your password has been encoded and stored!")
+
+
+
