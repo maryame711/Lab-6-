b84d6ea00848789e2f06711441d5e52ae18c398b
maryame711 <149011568+maryame711@users.noreply.github.com>
2023-10-25 16:51:21 -0400

    Add files via upload

--- maryam.py
+++ maryam.py
@@ -21,31 +21,6 @@ def menu():
 
 
 
-def decode(password):
-    decode_list =[]
-    for digit in password:
-        decode_list.append(int(digit))
-
-    ranng = len(decode_list)
-
-    for i in range(0,ranng):
-        decode_list[i] = decode_list[i] - 3
-
-    for i in range(0,ranng):
-        decode_list[i] = str(decode_list[i])
-
-    decoded_pass = "".join(decode_list)
-    print(decoded_pass)
-
-
-
-menu()
-#option = int(input("Please enter an option")
-passwrd = input("Please enter your password to encode:")
-
-decode(passwrd)
-
-
 
 
 
